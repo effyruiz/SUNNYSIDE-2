03ee7468583d0281d57a29c74c1a7ed20c408c97
JuanSoto18 <juanvidalsototovar@gmail.com>
2023-03-18 17:40:15 -0400

    menu fixes

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-// Asi se escribe un comentario
-console.log("objeto comentario", document);
+// PASOS PARA EL HAMBURGER
 
-// Atrapa el elemento del dom (elemento renderizado en la web)
-const menuObject = document.getElementById('menuHam');
+// 1. OBTENER EL DOM ELEMENT DEL BOTON CON ICONO Y DEL MENU HAMBURGUESA
+const btnHam = document.querySelector('.ham-btn');
+const hamMenu = document.querySelector('#menuHam');
 
-// Una vez tengamos el objecto dom/html podemos modificarlo, agregar propiedades, clases y alterar su estilo
-menuObject.classList.add('hide');
+// 2. AGREGAR UNA CLASE PARA OCULTAR EL MENU CUANDO CARGA LA PAGINA
+hamMenu.classList.add('hide');
 
-// cambiar el color del elemento
-menuObject.style.color = 'green';
+// 3. AGREGAR UN EVENTO AL BOTON DOM CADA VEZ QUE SE HACE CLICK A EL
+btnHam.addEventListener('click', (e) => toggleMenu(e));
 
-// Agregar eventos a elementos html
-
-// capturar el elemento al que se le agrega el evento (accediendo por clases como css)
-const headerHam = document.querySelector('.header-ham .hamburguer-menu');
-
-// Al hacer click llama a una funcion
-headerHam.addEventListener('click', function(e) {
-    console.log("le diste click a este elemento dom", e.target);
-});
+// 4. DESPUES DE HACE CLICK AL BOTON SE DEBERIA MOSTRAR/OCULTAR EL MENU DE HAMBURGUESA
+function toggleMenu(e) {
+    e.preventDefault();
+    hamMenu.classList.toggle('hide');
+}
